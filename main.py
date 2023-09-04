e4a06dc338160f6da73bb4e7c0caed7eb2fdd04d
Aman Kapoor <akalasklo@gmail.com>
2023-09-04 12:04:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from textblob import TextBlo
 app = FastAPI()
 
 # Example route
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-@app.get("/sentiment_en")
-def sentiment(text):
+class SentimentRequest(BaseModel):
+    text:str
+@app.post("/analyze_sentiment")
+async def analyze_sentiment(request: SentimentRequest):
+    text = request.text
     t1 = TextBlob(text)
-    sent = t1.polarity
-    if sent<0:
-        em = "Negative"
-    elif sent==0:
-        em = "Neutral"
+    polarity = t1.sentiment.polarity
+
+    if polarity < 0:
+        sentiment = "Negative"
+    elif polarity == 0:
+        sentiment = "Neutral"
     else:
-        em = "Postive"
-    return (em,sent)
+        sentiment = "Positive"
+
+    return {"sentiment": sentiment, "polarity": polarity}
