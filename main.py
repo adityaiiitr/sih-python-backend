3fb2fe89b8243f6bf3efc9b829a4442b440a33bd
Aman kapoor <akalasklo@gmail.com>
2023-09-04 12:40:54 +0530

    sentiments analysis english

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+from textblob import TextBlob
+
 app = FastAPI()
 
 # Example route
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+class SentimentRequest(BaseModel):
+    text:str
+
+@app.post("/analyze_sentiment")
+async def analyze_sentiment(request: SentimentRequest):
+    text = request.text
+    t1 = TextBlob(text)
+    polarity = t1.sentiment.polarity
+
+    if polarity < 0:
+        sentiment = "Negative"
+    elif polarity == 0:
+        sentiment = "Neutral"
+    else:
+        sentiment = "Positive"
+
+    return {"sentiment": sentiment, "polarity": polarity}
