581114dc119900749cb4f835e254e6d4e9f4438f
Aman Kapoor <akalasklo@gmail.com>
2023-09-04 11:40:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+@app.get("/sentiment_en")
+def sentiment(text):
+    t1 = TextBlob(text)
+    sent = t1.polarity
+    if sent<0:
+        em = "Negative"
+    elif sent==0:
+        em = "Neutral"
+    else:
+        em = "Postive"
+    return (em,sent)
